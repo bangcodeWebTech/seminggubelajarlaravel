9271ec09f320a16b2c2f857dcc299fbfff1d028b
Wahyu Firmansyah <muhammadwahyufirmansyah@gmail.com>
2018-08-29 18:38:23 +0700

    hari 2 done

--- learn/routes/web.php
+++ learn/routes/web.php
@@ -15,12 +15,38 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
-Route::get('/about', function() {
-	return view('about');
-});
+Route::get('/about', 'MyController@showAbout');
 
 
 Route::get('/testmodel', function() {
-	$query =  App\Post::all();                      ; 
-	return $query;
+	/*Mencari semua model*/
+	// $query =  App\Post::all();
+
+	/*Mencari model berdasarkan id*/
+	// $query = App\Post::find(1);
+
+	/*Mencari model berdasarkan title*/
+	// $query= App\Post::where('title','like','%cepat nikah%')->get();
+	// return $query;
+
+	/*mengubah record*/
+	// $post = App\Post::find(1);
+	// $post->title="Ciri keluarga sakinah";
+	// $post->save();
+	// return $post; 
+
+	/*Menghapus record*/
+	// $post = App\Post::find(1);
+	// $post->delete();
+
+	/*Menambah Record*/
+	$post= new App\Post;
+	$post->title="7 Amalan pembuka jodoh";
+	$post->content = "shalat malam, sedekah, puasa sunah, silaturahmi, senyum, doa, tobat";
+	$post->save();
+	return $post;
+	
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
